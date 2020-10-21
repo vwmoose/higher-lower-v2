8e82c60682a9b5dc72a747a68b4995782c67a710
Phil Rowley <phil@netsketch.co.uk>
2020-10-21 11:52:33 +0100

    Initial commit.

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Session;
+
+class CardController extends Controller
+{
+    // define a value for each card in the deck - doesnt matter which suite
+    private $suits			= ['hearts', 'diamonds', 'clubs', 'spades'];
+    private $deck_values	= ['1' => 'A', '2' , '3', '4', '5', '6', '7', '8', '9', '10', '11' => 'J', '12' => 'Q', '13' => 'K'];
+    private $new_card;
+    private $cards_left;
+
+    public function __construct()
+    {
+        // check to see if a deck exists
+        if (!Session::get('deck'))
+        {
+            // reset high score
+            Session::put('high_score',	0);
+
+            // save
+            Session::save();
+
+            // shuffle deck
+            $this->shuffleDeck();
+        }
+    }
+
+    /**
+    * return the deck as json
+    */
+    public function deck()
+    {
+        // define
+        $deck = collect([]);
+
+        // iterate over the suits
+        foreach($this->suits as $suit)
+        {
+            // iterate over values
+            foreach($this->deck_values as $value)
+            {
+                $deck->push([
+                    'value' => $value,
+                    'suit'  => $suit,
+                ]);
+            }
+        }
+
+        // return
+        return response()->json($deck->toArray());
+    }
+
+    // initial view
+    public function index()
+    {
+        // get card
+        $this->getCard();
+
+        // view
+        return view('game', ['card'			=> $this->new_card['face'],
+                            'card_suit'	    => $this->new_card['suit'],
+                            'cards_left'	=> $this->cards_left,
+                            'deck'			=> print_r(Session::get('deck'), true),
+                            'score'		    => Session::get('score'),
+                            'lives'		    => Session::get('lives'),
+                            'high_score'	=> Session::get('high_score'),
+                            'debug'		    => 'new game']);
+    }
+
+    // shuffle deck
+    public function shuffleDeck()
+    {
+        // iterate over the four suits
+        foreach ($this->suits as $suit) {
+
+            // produce 13 cards for each suit
+            foreach($this->deck_values AS $key => $value) {
+
+                // produce the 13 cards
+                $deck[] = array('suit' => $suit, 'value' => $key, 'face' => $value);
+            }
+        }
+
+        // shuffle them
+        shuffle($deck);
+
+        // define session
+        Session::put('deck',		$deck);
+        Session::put('card',		'');
+        Session::put('card_value',	'');
+        Session::put('card_suit',	'');
+        Session::put('cards_left',	52);
+        Session::put('score',		0);
+        Session::put('lives',		3);
+
+        // save
+        Session::save();
+
+        // redirect - new game
+        return redirect()->route('index');
+    }
+
+    // new card is higher than the existing card
+    public function higher()
+    {
+        // retrieve current card
+        $current_card = Session::get('card_value');
+
+        // get new card
+        $this->getCard();
+
+        // check for success
+        if ($this->new_card['value'] > $current_card)
+        {
+            // add a point to score
+            $this->addScore();
+
+        } else {
+
+            // remove a life
+            $this->useLife();
+        }
+
+        // view
+        return view('game', ['card'			=> $this->new_card['face'],
+                            'card_suit'	    => $this->new_card['suit'],
+                            'cards_left'	=> $this->cards_left,
+                            'deck'			=> print_r(Session::get('deck'), true),
+                            'score'		    => Session::get('score'),
+                            'lives'		    => Session::get('lives'),
+                            'high_score'	=> Session::get('high_score'),
+                            'debug'		    => $this->new_card['face'] . ' > ' . $current_card]);
+    }
+
+    // new card is lower than the existing card
+    public function lower()
+    {
+
+        // retrieve current card
+        $current_card = Session::get('card_value');
+
+        // get new card
+        $this->getCard();
+
+        // check for success
+        if ($this->new_card['value'] < $current_card)
+        {
+            // add a point to score
+            $this->addScore();
+
+        } else {
+
+            // remove a life
+            $this->useLife();
+        }
+
+        // view
+        return view('game', ['card'			=> $this->new_card['face'],
+                            'card_suit'	    => $this->new_card['suit'],
+                            'cards_left'	=> $this->cards_left,
+                            'deck'			=> print_r(Session::get('deck'), true),
+                            'score'		    => Session::get('score'),
+                            'lives'		    => Session::get('lives'),
+                            'high_score'	=> Session::get('high_score'),
+                            'debug'		    => $this->new_card['face'] . ' > ' . $current_card]);
+    }
+
+    // get next card
+    private function getCard()
+    {
+        // retrieve session
+        $deck				= Session::get('deck');
+        $this->cards_left	= Session::get('cards_left');
+
+        // pick next card
+        $this->new_card			= array_pop($deck);
+        $this->cards_left--;
+
+        // update session
+        Session::put('card',		$this->new_card['face']);
+        Session::put('card_value',	$this->new_card['value']);
+        Session::put('card_suit',	$this->new_card['suit']);
+        Session::put('cards_left',	$this->cards_left);
+        Session::put('deck',		$deck);
+
+        // save session
+        Session::save();
+    }
+
+    private function addScore()
+    {
+
+        // retrieve current score
+        $score = (int) Session::get('score');
+
+        // add point
+        $score++;
+
+        // check for no cards left
+        if (count(Session::get('deck')) == 0) {
+
+            // update high score
+            Session::put('high_score', Session::get('score'));
+        }
+
+        // update session
+        Session::put('score', $score);
+
+        // save session
+        Session::save();
+    }
+
+    private function useLife()
+    {
+
+        // retrieve current lives
+        $lives = (int) Session::get('lives');
+
+        // remove life
+        $lives--;
+
+        // check for no lives left
+        if ($lives == 0) {
+
+            // check for current score beats the high score
+            if (Session::get('score') > Session::get('high_score')) {
+
+                // update high score
+                Session::put('high_score', Session::get('score'));
+            }
+        }
+
+        // update session
+        Session::put('lives', $lives);
+
+        // save session
+        Session::save();
+    }
+}
---
+++ resources/views/game.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .card {
+            font-size: 25px;
+            padding: 10px;
+            width: 240px;
+            border: 1px solid #5e5e5e;
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+            border-radius: 5px;
+            -webkit-box-shadow: 5px 5px 5px #888888;
+            -moz-box-shadow: 5px 5px 5px #888888;
+            box-shadow: 5px 5px 5px #888888;
+            margin-bottom: 10px;
+        }
+
+        .card > .card_top_left {
+
+            padding-right: 85%;
+        }
+
+        .card > .card_bottom_right {
+
+            padding-left: 85%;
+            -moz-transform: scale(1, -1);
+            -webkit-transform: scale(1, -1);
+            -o-transform: scale(1, -1);
+            -ms-transform: scale(1, -1);
+            transform: scale(1, -1);
+        }
+
+        .card > .card_top_left > img, .card > .card_bottom_right > img {
+
+            width: 25px;
+        }
+
+        .card > .card_suit {
+            margin: 50px 0px;
+        }
+
+        .card > .card_suit > img {
+            width: 90px;
+            height: 90px;
+        }
+
+        .score {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+
+        .high_score {
+            font-size: 18px;
+            margin-bottom: 20px;
+        }
+
+        .instructions {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 20px
+        }
+
+        .row {
+            margin: 10px;
+
+        }
+    </style>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+
+            <div class="content">
+
+                <div class="score">
+
+                    @if ($lives > 0 && $cards_left > 0)
+
+                        Score = {{ $score }}&nbsp;&nbsp;&nbsp;Lives = {{ $lives }}
+
+                    @elseif ($cards_left == 0)
+
+                        YOU WIN!!!!
+
+                    @else
+
+                        YOU LOSE!
+
+                    @endif
+
+                </div>
+
+                <div class="high_score">High Score = {{ $high_score }}</div>
+
+                <div class="card">
+
+                    <div class="card_top_left">
+                        {!! $card !!}<br/>
+                        <img src="/img/{!! $card_suit !!}.png" class="card_suit"/>
+                    </div>
+
+                    <div class="card_suit">
+
+                        <img src="/img/{!! $card_suit !!}.png" />
+
+                    </div>
+
+                    <div class="card_bottom_right">
+                        {!! $card !!}<br/>
+                        <img src="/img/{!! $card_suit !!}.png" class="card_suit"/>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="content">
+
+                <form id="higherlower">
+
+                    @if ($lives > 0 && $cards_left > 0)
+
+                        <div class="instructions">Will the next card be higher or lower than the one above?</div>
+
+                        <a href="{{ route('higher') }}" class="btn btn-primary" role="button">Higher</a>&nbsp;<a href="{{ route('lower') }}" class="btn btn-success" role="button">Lower</a>
+
+                    @endif
+
+                    <br/><br/><a href="{{ route('shuffle') }}" class="btn btn-warning btn-xs" role="button">Shuffle Deck</a>
+
+                </form>
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+</body>
+</html>
